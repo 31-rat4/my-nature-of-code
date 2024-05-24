1ab7b01fb78107a8e8cb53b892ef3c4a34a4ba5b
Rodrigo Monteiro Marzullo <rodrigo.marzullo@gmail.com>
2024-05-24 01:55:02 -0300

    random step size

--- src/my-nature-of-code/0_random/0.4_gaussian/sketch.js
+++ src/my-nature-of-code/0_random/0.4_gaussian/sketch.js
@@ -2,8 +2,8 @@ function getRandomInt(max) {
 	return Math.floor(Math.random() * max);
 }
 
-function drawTheGauss(sd) {
-	//sd = standart deviation
+function drawTheGauss(sigma) {
+	//sigma = standart deviation
 	const heights = [];
 	const e = 2.71828183;
 	let m = 0;
@@ -11,8 +11,8 @@ function drawTheGauss(sd) {
 		let xcoord = map(i, 0, width, -2, 2);
 		let sq2pi = sqrt(2 * PI); //square root of 2 * PI
 		let xmsq = -1 * (xcoord - m) * (xcoord - m); //-(x - mu)^2
-		let sdsq = sd * sd; //variance (standard deviation squared)
-		heights[i] = (1 / (sd * sq2pi)) * pow(e, xmsq / sdsq); //P(x) function
+		let sdsq = sigma * sigma; //variance (standard deviation squared)
+		heights[i] = (1 / (sigma * sq2pi)) * pow(e, xmsq / sdsq); //P(x) function
 	}
 
 	// a little for loop that draws a line between each point on the graph
---
+++ src/my-nature-of-code/0_random/0.6_random_walker_vary/sketch.js
@@ -0,0 +1,34 @@
+class Walker {
+
+	constructor(size, velocity) {
+
+		this.x = width / 2;
+		this.y = height / 2;
+		this.velocity = velocity;
+		this.size = size;
+		this.step_size = 20;
+	}
+	draw() {
+		stroke(0);
+		ellipse(this.x, this.y, this.size, this.size);
+	}
+	step() {
+
+		this.x += random(-1, 1) * this.step_size;
+		this.y += random(-1, 1) * this.step_size;
+	}
+}
+let walker
+function setup() {
+	createCanvas(500, 500);
+	walker = new Walker(2, 2);
+	background(255);
+	// put setup code here
+}
+
+function draw() {
+	// put drawing code here
+	walker.step();
+	walker.draw();
+
+}
